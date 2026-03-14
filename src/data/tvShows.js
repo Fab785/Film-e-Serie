e768416d7c2e4fb70c5ed16c6cd19e5790205fdc
Fabrizio <fabrizioterribile@gmail.com>
2026-03-14 04:41:05 -0400

    new serie

--- src/data/tvShows.js
+++ src/data/tvShows.js
@@ -25,6 +25,14 @@ const tvShows = [
       rating: 5,
       watchedBy: ["fabrizio"]
     },
+    {
+      id: 4,
+      title: "Monarch: Legacy of Monsters",
+      image: "https://image.tmdb.org/t/p/w1280/jBn4LWlgdsf6xIUYhYBwpctBVsj.jpg",
+      note: "Una bella serie sul mondo di Godzilla, ben fatta",
+      rating: 5,
+      watchedBy: ["fabrizio"]
+    },
   ]
   
   export default tvShows
